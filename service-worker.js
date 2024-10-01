b01baecf15c984b344de271fb4e3e9563d228556
YamaKos <148765327+YamaKos@users.noreply.github.com>
2024-10-02 00:54:56 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('trip-guide-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/trip-guide.pdf',
+                '/manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
